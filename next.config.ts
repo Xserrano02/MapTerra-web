606d144db490b391564f5753df934b50c812e802
Xserrano02 <manu.fcbreyes@gmail.com>
2025-08-08 10:21:29 -0600

    refactor: remove unused components and files
    - Deleted the main page component (page.tsx) and its associated components including AnimatedSection, BannerInit, Carrusel, Details, Nosotros, Formulario, Footer, Menu, and Servicios.
    - Removed utility files such as Toaster, button, carousel, and utility functions.
    - Cleaned up the project structure by removing the tsconfig.json file.

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["m.media-amazon.com", "img.freepik.com",  "i.blogs.es", "grupotrato.com", "www.consumer.es", "i0.wp.com"],
-  },
-};
-
-module.exports = nextConfig;
